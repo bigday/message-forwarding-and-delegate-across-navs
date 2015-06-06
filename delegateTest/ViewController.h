aacf9c2941c0750f0cb55b88e647ae7863c6fd39
SunChao <sc@SunChaodeMacBook-Pro.local>
2015-06-06 14:20:30 +0800

    Initial Commit

---
+++ delegateTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  delegateTest
+//
+//  Created by SunChao on 15/6/6.
+//  Copyright (c) 2015年 SunChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ delegateTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  delegateTest
+//
+//  Created by SunChao on 15/6/6.
+//  Copyright (c) 2015年 SunChao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
